4850bd04fdf150125488dd58e7b565270dbff21a
M Kellen Baker <mkellenbaker@Ms-MacBook-Pro.local>
2021-11-21 13:44:11 -0600

    init

--- word-count.js
+++ word-count.js
@@ -3,6 +3,23 @@
 // convenience to get you started writing code faster.
 //
 
-export const countWords = () => {
-  throw new Error('Remove this statement and implement this function');
+export const countWords = (phrase) => {
+  phrase = phrase.replace(/[!@#$%&*();:.^]/g, '')
+  let words = phrase.split(/[\s,]/)
+
+  let obj = {}
+  words.forEach(word => {
+    word = word.toLowerCase()
+    if (word[0] === "'") {
+      word = word.substring(1, word.length -1)
+    }
+    if (word !== '') {
+      if (!obj[word]) {
+      obj[word] = 1
+    } else {
+      obj[word] = obj[word] + 1 
+    }
+    }
+  })
+  return obj
 };
